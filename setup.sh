977b33c0ad2fe9363e6721ed9c3aec0358efb4f3
Benjamin Yu <benjamin.yu741@gmail.com>
2019-03-18 15:59:02 -0400

    Set up git config

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ mv ~/.bash_profile trash
 cp vimrc ~/.vimrc
 cp bashrc ~/.bashrc
 cp bash_profile ~/.bash_profile
+git config --global user.name "Benjamin Yu"
+git config --global user.email "benjamin.yu741@gmail.com"
